98543ded2f322ace3151f300814f96a9db465016
AdrieanKhisbe <adriean.khisbe@live.fr>
2016-01-14 16:07:57 +0100

    Tweak and fixes

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
-if [ ! -d "./node_modules/jshint" ]; then
-  npm install jshint@2
-fi
-
-if [ ! -d "./node_modules/docco" ]; then
-  npm install docco@0
-fi
+#!/bin/sh
 
 ./node_modules/.bin/jshint eraro.js
 ./node_modules/.bin/docco eraro.js -o doc
--- test.sh
+++
@@ -1,5 +0,0 @@
-if [ ! -d "./node_modules/mocha" ]; then
-  npm install mocha@1
-fi
-
-node test/eraro.test.js
