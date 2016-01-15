e9ea7aa447318b54f957fe8c7fb86ffbf45ebf5f
AdrieanKhisbe <adriean.khisbe@live.fr>
2016-01-15 09:00:27 +0100

    Reformating of tests

--- test/eraro.test.js
+++ test/eraro.test.js
@@ -1,7 +1,7 @@
 /* Copyright (c) 2014 Richard Rodger, MIT License */
 'use strict';
 
-var util   = require('util')
+var util = require('util')
 var Lab = require('lab')
 var Code = require('code')
 
@@ -12,22 +12,26 @@ var expect = Code.expect
 
 var Eraro = require('..')
 var eraro = Eraro({
-    package:'foo',
-    msgmap:{
+    package: 'foo',
+    msgmap: {
         c401: 'C401 Message'
     }
 })
 
 function describeError(err) {
-    var out = { message: err.message, code: err.code,
-                package: err.package, msg: err.msg, details: err.details}
-    return out;
+    return {
+        message: err.message,
+        code: err.code,
+        package: err.package,
+        msg: err.msg,
+        details: err.details
+    }
 }
 
-describe('eraro', function(){
+describe('eraro', function () {
 
-    it('basic error', function(done){
-        var e1 = eraro('c1','m1 a:<%=a%>',{a:1})
+    it('basic error', function (done) {
+        var e1 = eraro('c1', 'm1 a:<%=a%>', {a: 1})
 
         expect(e1.message).to.equal('foo: m1 a:1')
         expect(e1.package).to.equal('foo')
@@ -41,7 +45,7 @@ describe('eraro', function(){
 
     describe('several error definitions', function (done) {
 
-     it('with prefix and context', function (done) {
+        it('with prefix and context', function (done) {
             var erraror = eraro('c2', {a: 2})
             expect(describeError(erraror)).to.deep.equal({
                 message: 'foo: c2', code: 'c2',
@@ -81,7 +85,7 @@ describe('eraro', function(){
 
     describe('wrap an error in details', function () {
 
-     it('simple error', function (done) {
+        it('simple error', function (done) {
             var err = new Error('x0')
             var erraror = eraro(err)
 
@@ -142,26 +146,26 @@ describe('eraro', function(){
         })
     })
 
-    it('handle templates', function(done){
+    it('handle templates', function (done) {
 
-        expect(eraro('b0','<%=foo(1)%>').message).to
+        expect(eraro('b0', '<%=foo(1)%>').message).to
             .contain("foo: <%=foo(1)%> VALUES:{ code: \'b0\' } TEMPLATE ERROR: TypeError:")
             .and.contain("is not a function")
 
-        var templatedError = eraro('i0','<%=a%>', {a: {b: 99 }})
+        var templatedError = eraro('i0', '<%=a%>', {a: {b: 99}})
         expect(templatedError.message).to.equal('foo: { b: 99 }')
 
         done()
     })
 
 
-    it('handle different erraros', function(done){
-        var fooEraro = Eraro({ package:'barfoo', prefix:'FOO-' })
+    it('handle different erraros', function (done) {
+        var fooEraro = Eraro({package: 'barfoo', prefix: 'FOO-'})
         expect(fooEraro('code0').message).to.equal('FOO-code0')
 
-       var barEraro = Eraro({ package:'bar', prefix:false })
-       expect(barEraro('code1').message).to.equal('code1')
+        var barEraro = Eraro({package: 'bar', prefix: false})
+        expect(barEraro('code1').message).to.equal('code1')
 
-       done()
+        done()
     })
 })
